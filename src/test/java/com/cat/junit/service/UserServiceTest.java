97d08d37cc56718181a11c1867924466cd97d9e5
Catherine <sunny4evil@gmail.com>
2023-04-02 11:52:15 +0300

    add before each and after each sections

--- src/test/java/com/cat/junit/service/UserServiceTest.java
+++ src/test/java/com/cat/junit/service/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.cat.junit.service;
 
 import com.cat.junit.entity.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,20 +12,35 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserServiceTest {
 
+    UserService userService;
+
+
+    @BeforeEach
+    void setUserService() {
+        System.out.println("Before Each:" + this);
+        userService = new UserService();
+
+    }
+
     @Test
     void emptyListIfNoUsers() {
-        UserService userService = new UserService();
+        System.out.println("EmptyListIfNoUsers:" + this);
         List<User> userList = userService.getAll();
         assertTrue(userList.isEmpty(), "Have to be empty");
     }
 
     @Test
     void sizeIfUsersAdded() {
-        UserService userService = new UserService();
+        System.out.println("SizeIfUsersAdded:" + this);
         userService.addUser(new User());
         userService.addUser(new User());
         List<User> userList = userService.getAll();
         int userListSize = userList.size();
         assertEquals(2, userListSize, "Have to be 2");
+    }
+
+    @AfterEach
+    void someCleanActions() {
+        System.out.println("AfterEach:" + this);
     }
 }
