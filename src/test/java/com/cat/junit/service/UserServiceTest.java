0ae70f614394ab3af54f8356e6427a4feaad06ca
Catherine <sunny4evil@gmail.com>
2023-04-07 20:13:41 +0300

    chance base junit asserts on assertj asserts

--- src/test/java/com/cat/junit/service/UserServiceTest.java
+++ src/test/java/com/cat/junit/service/UserServiceTest.java
@@ -1,10 +1,12 @@
 package com.cat.junit.service;
 
 import com.cat.junit.entity.User;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -27,7 +29,11 @@ public class UserServiceTest {
     void emptyListIfNoUsers() {
         System.out.println("EmptyListIfNoUsers:" + this);
         List<User> userList = userService.getAll();
-        assertTrue(userList.isEmpty(), "Have to be empty");
+
+        assertThat(userList)
+                .as("Have to be empty")
+                .isEmpty();
+//        assertTrue(userList.isEmpty(), "Have to be empty");
     }
 
     @Test
@@ -36,8 +42,12 @@ public class UserServiceTest {
         userService.addUser(new User());
         userService.addUser(new User());
         List<User> userList = userService.getAll();
-        int userListSize = userList.size();
-        assertEquals(2, userListSize, "Have to be 2");
+//        int userListSize = userList.size();
+
+        assertThat(userList)
+                .as("Have to be 2")
+                .hasSize(2);
+//        assertEquals(2, userListSize, "Have to be 2");
     }
 
     @AfterEach
