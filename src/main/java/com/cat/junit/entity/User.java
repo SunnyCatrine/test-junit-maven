63a7e84c763f3cf062601c7848e36761328e6af3
Catherine <sunny4evil@gmail.com>
2023-04-09 20:10:15 +0300

    add annotation AllargsConstructor to User class

--- src/main/java/com/cat/junit/entity/User.java
+++ src/main/java/com/cat/junit/entity/User.java
@@ -2,9 +2,11 @@ package com.cat.junit.entity;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
 public class User {
     private String name;
     private String password;
--- src/test/java/com/cat/junit/service/UserServiceTest.java
+++ src/test/java/com/cat/junit/service/UserServiceTest.java
@@ -24,7 +24,7 @@ public class UserServiceTest {
             .name("name")
             .password("password")
             .build();
-    
+
     private UserService userService;
 
     @BeforeAll
